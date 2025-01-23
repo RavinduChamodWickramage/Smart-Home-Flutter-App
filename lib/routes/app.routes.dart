0da49673c0a966487f2b53b9bbe2f2508eb425a9
Ravindu Chamod Wickramage <ravinduchamod1@gmail.com>
2025-01-23 13:11:06 +0530

    feat[ui]: add initial structure for features, routes, helpers, and styles

---
+++ lib/routes/app.routes.dart
@@ -0,0 +1,28 @@
+import 'package:go_router/go_router.dart';
+import '../features/intro/presentation/pages/loading.page.dart';
+import '../features/intro/presentation/pages/splash.page.dart';
+import '../helpers/utils.dart';
+
+class AppRoutes {
+  static final router = GoRouter(
+    routerNeglect: true,
+    initialLocation: SplashPage.route,
+    navigatorKey: Utils.mainNav,
+      routes: [
+        GoRoute(
+          parentNavigatorKey: Utils.mainNav,
+          path: SplashPage.route,
+          builder: (context, state) {
+            return const SplashPage();
+          },
+        ),
+        GoRoute(
+          parentNavigatorKey: Utils.mainNav,
+          path: LoadingPage.route,
+          builder: (context, state) {
+            return const LoadingPage();
+          },
+        ),
+      ]
+  );
+}
